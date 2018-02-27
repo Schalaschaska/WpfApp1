0dae696cb6eefd098e88f63723b671efb09e2f05
Schalaschaska <mihanhlystow@gmail.com>
2018-02-27 21:23:55 +0300

    Интерфейс меню

--- WpfApp1/Editor.xaml.cs
+++ WpfApp1/Editor.xaml.cs
@@ -30,7 +30,6 @@ namespace WpfApp1
             List<double> Ki_list = new List<double>();
             List<double> Di_lis = new List<double>();
             List<double> Ri_list = new List<double>();
-
             List<MyTable> result = new List<MyTable>(Convert.ToInt32(N_box.Text));
             
             for (int i = 1; i <=Convert.ToInt32(N_box.Text); i++)
